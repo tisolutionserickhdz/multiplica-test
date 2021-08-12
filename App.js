bb31e96d5e807b5e075fff833170d605d3fb2b86
tisolutionserickhdz <ti.solutions.erick.hdz@gmail.com>
2021-08-11 23:55:44 -0500

    ADD - Products and Services Flatlist added dinamycally by an array

--- App.js
+++ App.js
@@ -6,17 +6,13 @@
 import React,
 {
   useState,
-  useEffect,
-  useMemo,
-  useReducer
+  useEffect
 } from 'react';
 import {
   View,
   LogBox
 } from 'react-native';
 import GeneralStyles from './src/mx/multiplica/multiplica_app/styles/GeneralStyles';
-import { AuthContext } from './src/mx/multiplica/multiplica_app/components/GlobalContextComponent';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import LoaderIndicatorComponent from './src/mx/multiplica/multiplica_app/components/LoaderIndicatorComponent';
 import UnsignedUserNavigation from './src/mx/multiplica/multiplica_app/navigations/UnsignedUserNavigation';
@@ -46,7 +42,6 @@ const App = () => {
 
   // HOOKS
   const [loadingApp, setLoadingApp] = useState(true);
-  const [tokenStored, setTokenStored] = useState('');
 
   // ESTABLECE UN ESTADO DE INICIALIZACIÓN MIENTRAS FIREBASE SE CONECTA
   const [initializing, setInitializing] = useState(true);
@@ -61,91 +56,6 @@ const App = () => {
     setUser(usuario);
     if (initializing) setInitializing(false);
   }
-
-  // INICIALIZACION DE ESTADO DE VALORES PARA CONTEXT
-  const initialLoginState = {
-    userId: null,
-    userToken: null,
-    userEmail: null,
-    userPassword: null
-  }
-
-  // REDUCER PARA MANEJAR ACTIVACION DE ESTADO DEPENDIENDO VALORES PREDETERMINADOS
-  const loginReducer = (prevState, action) => {
-    switch (action.type) {
-      case 'RETRIEVE_TOKEN':
-        return {
-          ...prevState,
-          userToken: action.token
-        };
-      case 'LOGIN':
-        return {
-          ...prevState,
-          userId: action.id,
-          userToken: action.token,
-          userName: action.email,
-          userPassword: action.password
-        };
-      case 'LOGOUT':
-        return {
-          ...prevState,
-          userId: null,
-          userToken: null
-        };
-      case 'REGISTER':
-        return {
-          ...prevState,
-          userId: action.id,
-          userToken: action.token
-        };
-    }
-  }
-
-  // DECLARACION DE DISPATCHER PARA CAMBIAR VALOR DE ESTADO
-  const [loginState, dispatch] = useReducer(loginReducer, initialLoginState);
-
-  // CONTEXTO DE INICIO DE SESION, REGISTRO, RECUPERANDO TOKEN O SESION INICIADA
-  const authContext = useMemo(() => ({
-    signInContext: async (username, userpassword, accesstoken, userid) => {
-      setLoadingApp(true);
-      setTokenStored(accesstoken);
-      console.log('ENTRA A signInContext desde App.js');
-      console.log('userid: ', userid);
-      console.log('accesstoken: ', accesstoken);
-      console.log('username: ', username);
-      console.log('userpassword: ', userpassword);
-      if (accesstoken != undefined && accesstoken != null) {
-        await AsyncStorage.setItem('@userAuthTokenObject', accesstoken).then(() => {
-          console.log('GUARDADO DE OBJETO EXITOSO @userAuthTokenObject');
-          setLoadingApp(false);
-        }).catch(error => {
-          console.log('ERROR GUARDANDO OBJETO @userAuthTokenObject: ', error);
-        })
-      }
-      dispatch({ type: 'LOGIN', id: userid, token: accesstoken, email: username, password: userpassword });
-    },
-    signOutContext: async (accesstoken) => {
-      if (accesstoken != null) {
-        setTokenStored(accesstoken);
-        try {
-          // SE ELIMINA TODA LA INFO DE ASYNCSTORAGE
-          await AsyncStorage.multiRemove([
-            '@userAuthTokenObject',
-            '@loginInformationObject'
-          ]).then(() => {
-            console.log('INFO DE ASYNCSTORAGE ELIMINADA CORRECTAMENTE');
-          })
-        } catch (error) {
-          console.log("ERROR AL ELIMINAR LOS DATOS DEL ASYNCSTORAGE: ", error);
-        }
-
-        dispatch({ type: 'LOGOUT', id: null, token: null });
-      } else {
-        console.log('NO SE RECIBE ACCESS TOKEN');
-      }
-    }
-
-  }), []);
 
   useEffect(() => {
     setTimeout(() => {
@@ -165,22 +75,20 @@ const App = () => {
     );
   } else {
     return (
-      <AuthContext.Provider value={authContext}>
-        <NavigationContainer>
-          <StatusBarComponent
-            backgroundColor={RED_PRINCIPAL}
-            translucent={true}
-            statusBarFontStyle={'light-content'}
-            hidden={false}
-          />
-          {
-            user ?
-              <DrawerNavigation />
-              :
-              <UnsignedUserNavigation />
-          }
-        </NavigationContainer>
-      </AuthContext.Provider>
+      <NavigationContainer>
+        <StatusBarComponent
+          backgroundColor={RED_PRINCIPAL}
+          translucent={true}
+          statusBarFontStyle={'light-content'}
+          hidden={false}
+        />
+        {
+          user ?
+            <DrawerNavigation />
+            :
+            <UnsignedUserNavigation />
+        }
+      </NavigationContainer>
     );
   }
 };
